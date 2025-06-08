40df0294c026d679c4f0312598ce1061dde47071
VishaL6i9 <vishalkandakatla@gmail.com>
2025-06-08 23:38:14 +0530

    Added a basic container

---
+++ app/src/main/java/com/vishal/media3player/App.kt
@@ -0,0 +1,11 @@
+package com.vishal.media3player
+
+import android.app.Application
+import androidx.media3.common.util.UnstableApi
+
+@UnstableApi
+class MediaPlayerApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
