c7d56a8bb18e00a09da12c34fc94a6ef361b194f
VishaL6i9 <vishalkandakatla@gmail.com>
2025-06-09 05:45:54 +0530

    Updated Controller To Handle Uri As Parameter Instead Of Direct String

--- app/src/main/java/com/vishal/media3player/player/PlayerController.kt
+++ app/src/main/java/com/vishal/media3player/player/PlayerController.kt
@@ -1,13 +1,14 @@
 package com.vishal.media3player.player
 
 import android.content.Context
+import android.net.Uri
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 
 class PlayerController(context: Context) {
     val player = ExoPlayer.Builder(context).build()
 
-    fun play(uri: String) {
+    fun play(uri: Uri) {
         player.setMediaItem(MediaItem.fromUri(uri))
         player.prepare()
         player.play()
