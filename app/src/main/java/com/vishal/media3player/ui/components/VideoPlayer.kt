c7ed953c51c3beea2c824568b7da8b65581862c0
VishaL6i9 <vishalkandakatla@gmail.com>
2025-06-09 06:09:37 +0530

    Linked ui controls toggle to parameter ControllerVisibility

--- app/src/main/java/com/vishal/media3player/ui/components/VideoPlayer.kt
+++ app/src/main/java/com/vishal/media3player/ui/components/VideoPlayer.kt
@@ -1,5 +1,6 @@
 package com.vishal.media3player.ui.components
 
+import android.view.View
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -10,16 +11,19 @@ import androidx.media3.ui.PlayerView
 @Composable
 fun VideoPlayer(
     player: ExoPlayer,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    onControllerVisibilityChanged: (Boolean) -> Unit
 ) {
     AndroidView(
         factory = { ctx ->
             PlayerView(ctx).apply {
                 this.player = player
                 useController = true
-                // Optional: If you want the video to always fill the screen and be cropped if aspect ratios don't match, use RESIZE_MODE_ZOOM.
-                // Otherwise, RESIZE_MODE_FIT (default) maintains aspect ratio with black bars.
-                // resizeMode = com.google.android.exoplayer2.ui.AspectRatioFrameLayout.RESIZE_MODE_ZOOM
+                setControllerVisibilityListener(
+                    PlayerView.ControllerVisibilityListener {
+                        visibility -> onControllerVisibilityChanged(visibility == View.VISIBLE)
+                    }
+                )
             }
         },
         update = { playerView ->
